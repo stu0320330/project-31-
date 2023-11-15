b6250a70f03dbb61024c86f0c1584d736204d0b9
Savinu Nugith Dulwan Wimalasuriya <150595620+stu0320330@users.noreply.github.com>
2023-11-15 18:12:12 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (value))
+})
+let value = 0
+value = pins.analogReadPin(AnalogPin.P0)
+if (value < 100) {
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+}
+basic.pause(5000)
 basic.forever(function () {
 	
 })
